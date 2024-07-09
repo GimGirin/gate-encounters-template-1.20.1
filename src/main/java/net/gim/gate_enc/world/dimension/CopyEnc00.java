3474e3e6dd08c332bb17dae544e36e376852eb66
Jack Bagel <Ilysalhi22@gmail.com>
2024-07-09 14:49:34 -0400

    Fixed Copying Region Files to Dimensions

--- src/main/java/net/gim/gate_enc/world/dimension/CopyEnc00.java
+++ src/main/java/net/gim/gate_enc/world/dimension/CopyEnc00.java
@@ -1,35 +1,45 @@
 package net.gim.gate_enc.world.dimension;
 
-import com.google.common.io.Resources;
-import net.minecraft.resource.Resource;
-import net.minecraft.resource.ResourceFinder;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.WorldSavePath;
-import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public final class CopyEnc00 {
     public static void copyRegionToWorld(MinecraftServer server) throws IOException, URISyntaxException {
 
-        File path = new File(server.getSavePath(WorldSavePath.ROOT).resolve("dimensions/gate_enc/encounter_00/").toString());
+        File path = new File(server.getSavePath(WorldSavePath.ROOT).resolve("dimensions/gate_enc/encounter_00/region").toString());
 
+        // Creates the region folder if it doesn't exist yet
         if (!path.exists()) {
             path.mkdirs();
         }
 
-        String target = server.getSavePath(WorldSavePath.ROOT).resolve("dimensions/gate_enc/encounter_00/").toString();
+        Path TO = server.getSavePath(WorldSavePath.ROOT).resolve("dimensions/gate_enc/encounter_00/region");
+        Path FROM = Path.of("data/gate_enc/dimension/encounter_00/region/");
 
-        File TO = new File(target);
-//        File From =
-        File FROM = new File(Paths.get(CopyEnc00.class.getResource("/data/gate_enc/dimension/encounter_00/").toURI()).toString());
+        copyToRegionDir(TO, FROM);
 
-        System.out.println(CopyEnc00.class.getResource("/data/gate_enc/dimension/encounter_00/"));
+    }
 
-        FileUtils.copyDirectory(FROM, TO,true);
+    private static void copyToRegionDir(Path destination, Path from) throws IOException {
+        String[] regionIDs = {"r.0.0.mca", "r.0.-1.mca", "r.-1.0.mca", "r.-1.-1.mca", "r.-2.0.mca", "r.-2.-1.mca"};
+
+        for (String regionID : regionIDs) {
+            String resourcePath = from.toString() + "/" + regionID;
+            Path filePath = Path.of(destination.toString() + "/" + regionID);
+
+            // Read the region file inside the data folder and copy it to the region folder
+            try (InputStream stream = CopyEnc00.class.getClassLoader().getResourceAsStream(resourcePath)) {
+                Files.copy(stream, filePath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
